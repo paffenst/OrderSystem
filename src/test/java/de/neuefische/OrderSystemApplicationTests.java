f4f70d3cd7ccb6360f570bf18db1d94c8e2b27d4
paffen <pavel.stoyanov0@hm.edu>
2023-05-03 11:21:56 +0200

    Initial commit

---
+++ src/test/java/de/neuefische/OrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
